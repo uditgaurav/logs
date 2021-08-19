4f7d996d4d70c140632785d11a1657b76cf3c547
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2021-08-19 13:40:33 +0530

    Create debug.sh

---
+++ temp/debug.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "#############################################################"
+echo "############ Getting pods in Chaos Namespace ################"
+echo "#############################################################"
+echo 
+echo "kubectl get pods -n ${CHAOS_NAMESPACE}"
+kubectl get pods -n ${CHAOS_NAMESPACE} || true
+
+echo "###################################################################"
+echo "############ Getting pods in Application Namespace ################"
+echo "###################################################################"
+echo
+echo "kubectl get pods -n ${APP_NS}"
+kubectl get pods -n ${APP_NS} || true
+
+echo "################################################################"
+echo "############ Getting Chaos Resource Information ################"
+echo "################################################################"
+echo
+echo "kubectl get chaosengine,chaosexperiment,chaosresult -n ${CHAOS_NAMESPACE}"
+kubectl get chaosengine,chaosexperiment,chaosresult -n ${CHAOS_NAMESPACE} || true
+
+echo "################################################################"
+echo "############ Getting Service Account Information ################"
+echo "################################################################"
+echo
+echo "kubectl get sa -n ${CHAOS_NAMESPACE}"
+kubectl get sa -n ${CHAOS_NAMESPACE} || true
+
+echo "###################################################"
+echo "############ Describe ChaosEngine  ################"
+echo "###################################################"
+echo
+echo "kubectl describe chaosengine -n ${CHAOS_NAMESPACE}"
+kubectl describe chaosengine -n ${CHAOS_NAMESPACE} || true
+
+echo "###################################################"
+echo "############ Describe ChaosResult  ################"
+echo "###################################################"
+echo
+echo "kubectl describe chaosresult -n ${CHAOS_NAMESPACE}"
+kubectl describe chaosresult -n ${CHAOS_NAMESPACE} || true
+
+echo "####################################################"
+echo "############ Printing Operator logs ################"
+echo "####################################################"
+echo
+operator_name=$(kubectl get pods -n litmus -l app.kubernetes.io/component=operator --no-headers | awk '{print$1}')
+kubectl logs operator_name -n litmus > logs.txt
+cat logs.txt
