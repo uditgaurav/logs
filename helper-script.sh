dbd3f6da73d2fac241dfe244ac4251ccc9d9cc4f
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2023-09-18 23:45:48 +0530

    Update helper-script.sh

--- helper-script.sh
+++ helper-script.sh
@@ -4,15 +4,18 @@ abort() {
   echo "[ABORT]: Killing process started because of terminated signal received"
   echo "[ABORT]: Chaos Revert Started"
   
+  cmd="systemctl"
+  [[ "$ENABLE_SUDO" == "true" ]] && cmd="sudo $cmd"
+
   if [[ "$MASK" == "enable" ]]; then
     echo "[Chaos]: Unmask the $SERVICE_NAME service"
-    systemctl unmask "$SERVICE_NAME" || {
+    $cmd unmask "$SERVICE_NAME" || {
       echo "[ABORT]: Error unable to unmask the service"
       exit 1
     }
   fi
 
-  systemctl start "$SERVICE_NAME" || {
+  $cmd start "$SERVICE_NAME" || {
     echo "unable to start the service"
     exit 1
   }
@@ -24,18 +27,21 @@ abort() {
 # trap abort signal
 trap "abort" SIGINT SIGTERM
 
+cmd="systemctl"
+[[ "$ENABLE_SUDO" == "true" ]] && cmd="sudo $cmd"
+
 echo "[Info]: Stopping service '$SERVICE_NAME' with mask '$MASK' and duration '$DURATION'"
 
 if [[ "$MASK" == "enable" ]]; then
   echo "[Chaos]: Mask the $SERVICE_NAME service"
-  systemctl mask "$SERVICE_NAME" || {
+  $cmd mask "$SERVICE_NAME" || {
     echo "Error: Unable to mask the service"
     exit 1
   }
 fi
 
 echo "[Chaos]: Stopping the $SERVICE_NAME service"
-systemctl stop "$SERVICE_NAME" || {
+$cmd stop "$SERVICE_NAME" || {
   echo "Error: Unable to stop the service"
   exit 1
 }
@@ -45,17 +51,16 @@ sleep "$DURATION"
 
 if [[ "$MASK" == "enable" ]]; then
   echo "[Chaos]: Unmask the $SERVICE_NAME service"
-  systemctl unmask "$SERVICE_NAME" || {
+  $cmd unmask "$SERVICE_NAME" || {
     echo "Error: Unable to unmask the service"
     exit 1
   }
 fi
 
 echo "[Chaos]: Starting the $SERVICE_NAME service"
-systemctl start "$SERVICE_NAME" || {
+$cmd start "$SERVICE_NAME" || {
   echo "Error: Unable to start the service"
   exit 1
 }
 
 echo "[Info]: The service started successfully"
-
